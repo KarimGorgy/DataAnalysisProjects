6e21aff69ad44ad2c4b1ab7d7afb969ead017e1d
Karim Gorgy <karimramzy2507@gmail.com>
2023-05-18 19:10:15 -0400

    added function automating calculation of weight of evidence for independent factor and IV

--- Course.py
+++ Course.py
@@ -70,3 +70,49 @@ loan_data['good_bad'] = np.where(loan_data['loan_status'].isin(['Charged Off','D
 
 # Split data into train and test
 loan_data_inputs_train,loan_data_inputs_test,loan_data_targets_train,loan_data_targets_test = train_test_split(loan_data.drop('good_bad',axis=1),loan_data['good_bad'],test_size=0.2,random_state = 42)
+
+
+df_inputs_prepr = loan_data_inputs_train
+df_targets_prepr = loan_data_targets_train
+df1 = pd.concat([df_inputs_prepr['grade'],df_targets_prepr],axis = 1)
+df1 = pd.concat([df1.groupby(df1.columns.values[0],as_index=False)[df1.columns.values[1]].count(),df1.groupby(df1.columns.values[0],as_index=False)[df1.columns.values[1]].mean()], axis=1)
+df1 =df1.iloc[:,[0,1,3]]
+df1.columns = [df1.columns.values[0],'n_obs','prop_good']
+
+df1['prop_n_obs'] = df1['n_obs'] / df1['n_obs'].sum()
+df1['n_good'] = df1['prop_good'] * df1['n_obs']
+df1['n_bad'] = (1-df1['prop_good']) * df1['n_obs']
+df1['prop_n_good'] = df1['n_good'] / df1['n_good'].sum()
+df1['prop_n_bad'] = df1['n_bad'] / df1['n_bad'].sum()
+df1['WoE'] = np.log(df1['prop_n_good']/df1['prop_n_bad'])
+df1 = df1.sort_values(['WoE'])
+df1 = df1.reset_index(drop=True)
+
+df1['diff_prop_good'] = df1['prop_good'].diff().abs()
+df1['diff_WoE'] = df1['WoE'].diff().abs()
+
+df1['IV'] = (df1['prop_n_good'] - df1['prop_n_bad']) * df1['WoE']
+df1['IV'] = df1['IV'].sum()
+print(df1)
+
+def woe_discrete(df,discrete_variable_name,good_bad_variable_df):
+    df = pd.concat([df[discrete_variable_name],good_bad_variable_df],axis=1)
+    df = pd.concat([df.groupby(df.columns.values[0],as_index=False)[df.columns.values[1]].count(),df.groupby(df.columns.values[0],as_index=False)[df.columns.values[1]].mean()], axis=1)
+    df = df.iloc[:,[0,1,3]]
+    df.columns = [df.columns.values[0],'n_obs','prop_good']
+
+    df['prop_n_obs'] = df['n_obs'] / df['n_obs'].sum()
+    df['n_good'] = df['prop_good'] * df['n_obs']
+    df['n_bad'] = (1-df['prop_good']) * df['n_obs']
+    df['prop_n_good'] = df['n_good'] / df['n_good'].sum()
+    df['prop_n_bad'] = df['n_bad'] / df['n_bad'].sum()
+    df['WoE'] = np.log(df['prop_n_good']/df['prop_n_bad'])
+    df = df.sort_values(['WoE'])
+    df = df.reset_index(drop=True)
+
+    df['diff_prop_good'] = df['prop_good'].diff().abs()
+    df['diff_WoE'] = df['WoE'].diff().abs()
+
+    df['IV'] = (df['prop_n_good'] -df['prop_n_bad']) * df['WoE']
+    df['IV'] = df['IV'].sum()
+    return df
